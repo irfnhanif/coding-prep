c9c44e45360e6d47e93ca7328c36f201bc27f603
irfnhanif <14.irfanh@gmail.com>
2024-11-04 10:59:04 +0700

    Solved one leetcode problem

--- Leetcode/Java/array-hashing/src/EasyReplaceElementsWithGreatestElementOnRightSide.java
+++ Leetcode/Java/array-hashing/src/EasyReplaceElementsWithGreatestElementOnRightSide.java
@@ -6,16 +6,14 @@ public class EasyReplaceElementsWithGreatestElementOnRightSide {
     }
 
     public static int[] replaceElements(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            int highest = 0;
-            for (int j = i+1; j < arr.length ; j++) {
-                if (arr[j] > highest) {
-                    highest = arr[j];
-                }
+        int max = -1;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int temp = arr[i];
+            arr[i] = max;
+            if (temp > max) {
+                max = temp;
             }
-            arr[i] = highest;
         }
-        arr[arr.length - 1] = -1;
 
         return arr;
     }
