3920eb111171df9083288e4787d22cea4ddbee94
irfnhanif <14.irfanh@gmail.com>
2024-11-05 15:27:14 +0700

    Solved one leetcode problem

---
+++ Leetcode/Java/array-hashing/src/EasyRemoveElement.java
@@ -0,0 +1,16 @@
+public class EasyRemoveElement {
+    public static void main(String[] args) {
+        System.out.println(removeElement(new int[]{3,2,2,3}, 3));
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
