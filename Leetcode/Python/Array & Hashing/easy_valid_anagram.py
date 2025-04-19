18d71e84546e078b3a0dd33edf41baab0f779b3a
irfnhanif <14.irfanh@gmail.com>
2025-04-19 16:45:51 +0700

    Solved one leetcode problem with python

--- Leetcode/Python/Array & Hashing/easy_valid_anagram.py
+++ Leetcode/Python/Array & Hashing/easy_valid_anagram.py
@@ -4,10 +4,8 @@ def isAnagram(s, t):
 
     for i in s:
         s_count[i] = (s_count.get(i, 0) + 1)
-        
+
     for j in t:
         t_count[j] = (t_count.get(j, 0) + 1)
     
     return s_count == t_count
-
-print(isAnagram("rat", "car"))
